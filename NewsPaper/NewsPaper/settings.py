e744841aa211c462985ca721bf51258c04358b1a
Shagi80 <shagi80@mail.ru>
2023-04-03 21:05:37 +0300

    Module D7.3.

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -189,3 +189,12 @@ APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
  
 # если задача не выполняется за 25 секунд, то она автоматически снимается, можете поставить время побольше, но как правило, это сильно бьёт по производительности сервера
 APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
+
+# ----------------------- настройка кэширования -------------------------------------
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), 
+    }
+}
